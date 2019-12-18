1e292fb8470d457247dc65e41f51b0e65dae6843
Jeffrey Martinez <martinezjf2@gmail.com>
2019-12-18 01:06:53 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,4 +1,4 @@
 def oxford_comma(array)
   return array.first if array.length == 1
-  return "#{array.first} and #{array.last}" if array.legth == 2
+  return "#{array.first} and #{array.last}" if array.length == 2
 end
